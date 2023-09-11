959d422db9145c284478b31ae3d4c8a2fd2cddbb
yohannesjoya <yohannesdestagebru10@gmail.com>
2023-09-12 01:12:31 +0300

    added a starter of Infrastructure layer of ordering service

---
+++ src/Services/Order/Ordering.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+
+        public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration) {
+
+            services.AddDbContext<OrderDbContext>(options =>
+               options.UseSqlServer(
+                   configuration.GetConnectionString("OrderConnectionString")));
+
+            return services;
+        
+        }
+    }
+}
---
+++ src/Services/Order/Ordering.Infrastructure/OrderDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure
+{
+    public class OrderDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(OrderDbContext).Assembly);
+
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "swn";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "swn";
+                        break;
+                }
+            }
+
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+    }
+
+
+}
